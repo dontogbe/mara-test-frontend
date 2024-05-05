a1d705aa1a03fbfa503cea1bcc6ac14afb466173
Albert Togbe <albert.togbe@frontdoorhome.com>
2024-05-05 09:55:45 +0000

    added http service

--- list-app/src/app/app.component.ts
+++ list-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ListsComponent } from './lists/lists.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule,ListsComponent],
+  imports: [RouterOutlet,FormsModule,HttpClientModule,ListsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
