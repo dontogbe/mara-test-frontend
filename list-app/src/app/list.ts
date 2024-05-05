adc91ff3ffd62f3668d675d5263aa7078170a751
Albert Togbe <albert.togbe@frontdoorhome.com>
2024-05-05 08:04:03 +0000

    made list of cards

---
+++ list-app/src/app/list.ts
@@ -0,0 +1,5 @@
+export interface List{
+    id:Number;
+    name:String;
+    description:String;
+}
--- list-app/src/app/lists/lists.component.ts
+++ list-app/src/app/lists/lists.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { List } from '../list';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-lists',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './lists.component.html',
   styleUrl: './lists.component.css'
 })
 export class ListsComponent {
-  name="test";
+  lists:List[]=[
+    {
+    id:1,
+    name:"test",
+    description:"more test"
+  },
+  {
+    id:2,
+    name:"test",
+    description:"more test"
+  },
+  {
+    id:3,
+    name:"test",
+    description:"more test"
+  },
+  {
+    id:4,
+    name:"test",
+    description:"more test"
+  },
+  {
+    id:5,
+    name:"test",
+    description:"more test"
+  },
+];
 }
